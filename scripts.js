ef003b54e8943c880b42641f63524e22d3fd0153
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-22 14:08:35 -0400

    Update and rename script.js to scripts.js

--- scripts.js
+++ scripts.js
@@ -2,27 +2,27 @@
 // TODO: Get references to the required DOM elements using document.getElementById()
 
 
-let posterQuote =document.getElementById("poster-quote");s
+let posterQuote =document.getElementById("poster-quote");
 let posterImage = document.getElementById("poster-image");
 let posterTitle = document.getElementById("poster-title");
 let randomize = document.getElementById("randomize");
 let submit = document.getElementById("submit");
 // Array of predefined poster objects
-const posters = [
+const randomposters = [
   {
-    image: 'image-url-1.jpg',
-    title: 'Poster 1',
-    quote: 'Quote 1',
+    image: 'https://i.etsystatic.com/27918564/r/il/23c05a/3057017349/il_fullxfull.3057017349_ir2d.jpg',
+    title: 'perserverance',
+    quote: 'stick to the coding.',
   },
   {
-    image: 'image-url-2.jpg',
-    title: 'Poster 2',
-    quote: 'Quote 2',
+    image: 'https://i.ebayimg.com/images/g/lIgAAOxye3BRwNTV/s-l1600.jpg',
+    title: 'Biking',
+    quote: 'Tire your legs',
   },
   {
-    image: 'image-url-3.jpg',
-    title: 'Poster 3',
-    quote: 'Quote 3',
+    image: 'https://i5.walmartimages.com/asr/b7ef2cef-2084-4a1c-87a6-4a540fd57522.0b3d8ee4f88465d291210574a7723aec.jpeg',
+    title: 'Fish',
+    quote: 'be bigger',
   },
   // Add more poster objects as needed
 ];
@@ -37,9 +37,9 @@ submit.addEventListener('click', generateCustomPoster);
 // Function to generate a random poster
 function generateRandomPoster() {
   // TODO: Generate a random index within the range of the posters array length
-let randomIndex = Math.floor(Math.random()*posters.length);  //math.floor(math random times array length)
+const randomIndex = Math.floor(Math.random()*randomposters.length);  //math.floor(math random times array length)
   // TODO: Retrieve the random poster object from the posters array
-let randomPoster = posters[randomIndex];
+const randomPoster = randomposters[randomIndex];
   // TODO: Call the function to update the DOM with the values from the random poster object
   updatePoster(randomPoster.image, randomPoster.title, randomPoster.quote); //from the posters array
 }
@@ -49,22 +49,25 @@ function generateCustomPoster(event) {
   event.preventDefault();
 
   // TODO: Retrieve the entered quote and author from the input fields
-let enteredQuote = document.getElementById("poster-form-quote");
-  let enteredAuthor = document.getElementById("poster-form-author");
+let enteredQuote = document.getElementById("poster-form-quote").value; //make sure u put a .value
+let enteredAuthor = document.getElementById("poster-form-author").value;
+let enteredURL = document.getElementById("poster-form-image").value;
   // TODO: Create a custom poster object with the entered values
-let customPoster = {
-  image: "https://media.defense.gov/2019/Oct/16/2002195533/-1/-1/0/430309-O-ZZ999-004.JPG",
+const customPoster = {
+  image: enteredURL,
   title: 'Your motivational poster',
-  quote: enteredQuote + ', written by ' + enteredAuthor;
+  quote: enteredQuote + ', written by ' + enteredAuthor
 }
   // TODO: Call the function to update the DOM with the values from the custom poster object
 updatePoster(customPoster.image, customPoster.title, customPoster.quote);
+  
 }
 
 // Function to update the poster content in the DOM
-function updatePoster(img, titl, quot) {
+
+function updatePoster(img, title, quote) {
   // TODO: Update the DOM with the values provided for the poster image, title, and quote
 posterImage.src = img;
-posterTitle.textContent = titl;
-posterQuote.textContent = quot;
+posterTitle.textContent= title;
+posterQuote.textContent= quote;
 }
